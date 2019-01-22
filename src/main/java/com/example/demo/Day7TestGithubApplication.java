22a1b7bf39739b92e98f015d08aa8164ed8247b5
yixiang_x <yixiang_x@163.com>
2019-01-22 09:38:39 +0800

    修改

--- src/main/java/com/example/demo/Day7TestGithubApplication.java
+++ src/main/java/com/example/demo/Day7TestGithubApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day7TestGithubApplication {
 
 	public static void main(String[] args) {
+		System.out.println("1231231");
 		SpringApplication.run(Day7TestGithubApplication.class, args);
 	}
 
